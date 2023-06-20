44d3ac7ad00642c238cefad7fbb1c845a878f82d
AshiqTasdid <admin@ashiqthedev.com>
2023-06-20 14:01:05 +0600

    Added Event Handler Executing Code in index.js

--- index.js
+++ index.js
@@ -32,4 +32,20 @@ for (const folder of commandFolders) {
 	}
 }
 
+// Event Handler
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+
 client.login(process.env.BOT_TOKEN);
