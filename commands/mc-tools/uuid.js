c072b8c639de2f237834e7576a33ab71451e70de
AshiqTasdid <admin@ashiqthedev.com>
2023-06-22 20:01:03 +0600

    Fixed UUID=undefined instead of error message

--- commands/mc-tools/uuid.js
+++ commands/mc-tools/uuid.js
@@ -29,95 +29,94 @@ module.exports = {
             .setURL('https://spectex.xyz/project/minecraft-utilities')
             .addFields(
               { name: `UUID of ${name}`, value: `\`\`\`${uuid}\`\`\`` },
-            );
+              );
+              if (uuid == null) {
+              interaction.editReply({
+                components: [
+                  {
+                    'type': 1,
+                    'components': [
+                      {
+                        'style': 5,
+                        'label': 'Vote',
+                        'url': 'https://top.gg/bot/810192936472936480/vote',
+                        'disabled': false,
+                        'type': 2,
+                      },
+                      {
+                        'style': 5,
+                        'label': 'Website',
+                        'url': 'https://spectex.xyz/projects/minecraft-utilities',
+                        'disabled': false,
+                        'type': 2,
+                      },
+                      {
+                        'style': 5,
+                        'label': 'Support Server',
+                        'url': 'https://discord.gg/jf28jcFJk9',
+                        'disabled': false,
+                        'type': 2,
+                      },
+                    ],
+                  },
+                ],
+                embeds: [
+                  {
+                    'type': 'rich',
+                    'title': 'Minecraft Utilities',
+                    'description': '**There has been an error**.',
+                    'color': 0xcb3333,
+                    'fields': [
+                      {
+                        'name': 'Make sure to check the following things:\n',
+                        'value': '\`\`\` 1. Check the Username. eg: Technoblade \`\`\`\n\`\`\` 2. Use the right command\neg: /skin for Skin & /UUID for UUID\`\`\`', // eslint-disable-line
+                      },
+                      {
+                        'name': 'Here is the reason you might be having trouble:',
+                        'value': '\`\`\` 1. Your Provided Username does not exist \`\`\`\n\`\`\` 2. There are some issues with the bot\nin that case, use /help\`\`\`', // eslint-disable-line
+                      },
+                    ],
+                  },
+                ],
+                ephemeral: true,
+              });
+            } else {
           interaction.editReply({
             components : [
+          {
+            'type': 1,
+            'components': [
               {
-                'type': 1,
-                'components': [
-                  {
-                    'style': 5,
-                    'label': 'Vote',
-                    'url': 'https://top.gg/bot/810192936472936480/vote',
-                    'disabled': false,
-                    'type': 2,
-                  },
-                  {
-                    'style': 5,
-                    'label': 'Website',
-                    'url': 'https://spectex.xyz/projects/minecraft-utilities',
-                    'disabled': false,
-                    'type': 2,
-                  },
-                  {
-                    'style': 5,
-                    'label': 'Support Server',
-                    'url': 'https://discord.gg/jf28jcFJk9',
-                    'disabled': false,
-                    'type': 2,
-                  },
-                ],
+                'style': 5,
+                'label': 'Vote',
+                'url': 'https://top.gg/bot/810192936472936480/vote',
+                'disabled': false,
+                'type': 2,
+              },
+              {
+                'style': 5,
+                'label': 'Website',
+                'url': 'https://spectex.xyz/projects/minecraft-utilities',
+                'disabled': false,
+                'type': 2,
+              },
+              {
+                'style': 5,
+                'label': 'Support Server',
+                'url': 'https://discord.gg/jf28jcFJk9',
+                'disabled': false,
+                'type': 2,
               },
             ],
-            embeds: [uuidEmbed],
-            ephemeral: true,
-          });
-          return;
+          },
+        ],
+        embeds: [uuidEmbed],
+        ephemeral: true,
+      });
+      return;
+        }
         })
         .catch((error) => {
-          interaction.editReply({
-            components: [
-              {
-                'type': 1,
-                'components': [
-                  {
-                    'style': 5,
-                    'label': 'Vote',
-                    'url': 'https://top.gg/bot/810192936472936480/vote',
-                    'disabled': false,
-                    'type': 2,
-                  },
-                  {
-                    'style': 5,
-                    'label': 'Website',
-                    'url': 'https://spectex.xyz/projects/minecraft-utilities',
-                    'disabled': false,
-                    'type': 2,
-                  },
-                  {
-                    'style': 5,
-                    'label': 'Support Server',
-                    'url': 'https://discord.gg/jf28jcFJk9',
-                    'disabled': false,
-                    'type': 2,
-                  },
-                ],
-              },
-            ],
-            embeds: [
-              {
-                'type': 'rich',
-                'title': 'Minecraft Utilities',
-                'description': '**There has been an error**.',
-                'color': 0xcb3333,
-                'fields': [
-                  {
-                    'name': 'Make sure to check the following things:\n',
-                    'value': '\`\`\` 1. Check the Username. eg: Technoblade \`\`\`\n\`\`\` 2. Use the right command\neg: /skin for Skin & /UUID for UUID\`\`\`', // eslint-disable-line
-                  },
-                  {
-                    'name': 'Here is the reason you might be having trouble:',
-                    'value': '\`\`\` 1. Your Provided Username does not exist \`\`\`\n\`\`\` 2. There are some issues with the bot\nin that case, use /help\`\`\`', // eslint-disable-line
-                  },
-                  {
-                    'name': 'Here is the error code',
-                    'value': `${error}}`,
-                  },
-                ],
-              },
-            ],
-            ephemeral: true,
-          });
           console.log(error);
         });
 	},
