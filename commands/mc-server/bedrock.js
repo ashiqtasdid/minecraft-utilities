074308f2230a8bd6a97be9ddf67ad732ff2ff0e4
ashiqtasdid <ashiqtasdid5@gmail.com>
2023-09-08 22:25:10 +0600

    Updated JAVA and BEDROCK

--- commands/mc-server/bedrock.js
+++ commands/mc-server/bedrock.js
@@ -1,137 +1,143 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const mcs = require('node-mcstatus');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 process.noDeprecation = true;
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('bedrock')
-    .setDescription('Get Real-Time info about a Minecraft: Bedrock Edition Server')
+    .setName("bedrock")
+    .setDescription("Get Real-Time info about a Minecraft: Bedrock Edition Server")
     .addStringOption((option) =>
       option
-        .setName('server-ip')
+        .setName("server-ip")
         .setRequired(true)
-        .setDescription('IP addess of the server'),
-    )
-    .addStringOption((option) =>
-      option
-      .setName('server-port')
-      .setDescription('Port of the server [Type Default for Default Port]')
-      .setRequired(true),
+        .setDescription("IP addess of the server")
     ),
-    async execute(interaction) {
 
-        await interaction.deferReply({ ephemeral: true });
+  async execute(interaction) {
+    await interaction.deferReply({ ephemeral: true });
 
-        let port = interaction.options.getString('server-port').toLowerCase();
-        const ip = interaction.options.getString('server-ip');
+    const ip = interaction.options.getString("server-ip");
+    const apiUrl = `https://api.mcsrvstat.us/bedrock/3/${ip}`;
 
-        if (port == 'default')
-        {
-            port = 19132;
-        }
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.online) {
+          console.log("[BEDROCK] " + ip);
 
-        mcs.statusBedrock(ip, port)
-
-        .then((result) => {
-            console.log(ip + ', ' + port);
-
-            const bedrockEmbed = new EmbedBuilder()
-            .setColor(0x31D533)
-            .setTitle('Minecraft Utilities')
-            .setURL('https://spectex.xyz/projects/minecraft-utilities')
-            .setDescription('**Here is the requested server status**')
+          const javaEmbed = new EmbedBuilder()
+            .setColor(0x31d533)
+            .setTitle("Minecraft Utilities")
+            .setURL("https://spectex.xyz/")
+            .setDescription("**Here is the requested server status**")
             .addFields(
-                { name: '**Players Online**', value: `\`\`\`${result.players.online}\`\`\`` },
-                { name: '**Max Player Cap**', value: `\`\`\`${result.players.max}\`\`\`` },
-                { name: '**Server Version**', value: `\`\`\`${result.version.name_clean}\`\`\`` },
-                { name: '**Online Mode**', value: `\`\`\`${result.online}\`\`\`` },
-                { name: '**Is The Eula Blocked ?**', value: `\`\`\`${result.eula_blocked}\`\`\`` },
-                { name: '**Mods, [ ] = No Mods**', value: `\`\`\`[${result.mods}]\`\`\`` },
-                { name: '**Motd**', value: `\`\`\`${result.motd.clean.trim()}\`\`\`` },
+              { name: "**Numeric IP**", value: `\`\`\`${data.ip}\`\`\`` },
+              { name: "**Server IP**", value: `\`\`\`${data.hostname}\`\`\`` },
+              {
+                name: "**Players Online - [Online / Max]**",
+                value: `\`\`\`${data.players.online} / ${data.players.max}\`\`\``,
+              },
+              { name: "**Version**", value: `\`\`\`${data.version}\`\`\`` },
+              {
+                name: "**Gamemode**",
+                value: `\`\`\`${data.gamemode || "Multiple"}\`\`\``,
+              },
+              {
+                name: "**MOTD**",
+                value: `\`\`\`${data.motd.clean}\`\`\``,
+              }
             );
-            interaction.editReply({
-                components : [
-                    {
-                      'type': 1,
-                      'components': [
-                        {
-                          'style': 5,
-                          'label': 'Vote',
-                          'url': 'https://top.gg/bot/810192936472936480/vote',
-                          'disabled': false,
-                          'type': 2,
-                        },
-                        {
-                          'style': 5,
-                          'label': 'Website',
-                          'url': 'https://spectex.xyz/projects/minecraft-utilities',
-                          'disabled': false,
-                          'type': 2,
-                        },
-                        {
-                          'style': 5,
-                          'label': 'Support Server',
-                          'url': 'https://discord.gg/jf28jcFJk9',
-                          'disabled': false,
-                          'type': 2,
-                        },
-                      ],
-                    },
-                  ],
-                embeds: [bedrockEmbed],
-            });
-            return;
-        })
-        .catch((error) => {
-            console.log('There has been an error');
-            interaction.editReply({
-              components: [
-                {
-                  'type': 1,
-                  'components': [
-                    {
-                      'style': 5,
-                      'label': 'Vote',
-                      'url': 'https://top.gg/bot/810192936472936480/vote',
-                      'disabled': false,
-                      'type': 2,
-                    },
-                    {
-                      'style': 5,
-                      'label': 'Website',
-                      'url': 'https://spectex.xyz/projects/minecraft-utilities',
-                      'disabled': false,
-                      'type': 2,
-                    },
-                    {
-                      'style': 5,
-                      'label': 'Support Server',
-                      'url': 'https://discord.gg/jf28jcFJk9',
-                      'disabled': false,
-                      'type': 2,
-                    },
-                  ],
-                },
-              ],
-              embeds: [
-                {
-                  'type': 'rich',
-                  'title': 'Minecraft Utilities',
-                  'description': '**There has been an error**',
-                  'color': 0xa92626,
-                  'fields': [
-                    {
-                      'name': 'Make sure to check the following things',
-                      'value': '\`\`\` 1. The IP of the Server. \neg: play.yourserver.com, 103.144.xxx.xxx \`\`\`\n\`\`\` 2. The PORT of the server. \neg: 19132 [Default].\`\`\`\n\`\`\` 3. Make sure to use the right command.\n/Java for Java Edition, /Bedrok for Bedrock.\`\`\`\n\n**Here are the reasons why your server might be having trouble**\n\`\`\` 1. Your Requested Server might be offline\`\`\`\n\`\`\` 2. Your Provided IP or PORT might be Incorrect\`\`\`\n\`\`\` 3. There are some issues with the bot, \nin that case, please use /help\`\`\`', // eslint-disable-line
-                    },
-                    {
-                      'name': 'Here is the error code',
-                      'value': `\`\`\`${error}\`\`\``,
-                    },
-                  ],
-                },
-              ],
-            });
-        });
-    },
+          interaction.editReply({
+            components: [
+              {
+                type: 1,
+                components: [
+                  {
+                    style: 5,
+                    label: "Vote",
+                    url: "https://top.gg/bot/810192936472936480/vote",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Website",
+                    url: "https://spectex.xyz/projects/minecraft-utilities",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Support Server",
+                    url: "https://discord.gg/jf28jcFJk9",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Documentation",
+                    url: "https://mcutils.spectex.xyz",
+                    disabled: false,
+                    type: 2,
+                  },
+                ],
+              },
+            ],
+            embeds: [javaEmbed],
+          });
+        } else {
+          interaction.editReply({
+            components: [
+              {
+                type: 1,
+                components: [
+                  {
+                    style: 5,
+                    label: "Vote",
+                    url: "https://top.gg/bot/810192936472936480/vote",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Website",
+                    url: "https://spectex.xyz/projects/minecraft-utilities",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Support Server",
+                    url: "https://discord.gg/jf28jcFJk9",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Documentation",
+                    url: "https://mcutils.spectex.xyz",
+                    disabled: false,
+                    type: 2,
+                  },
+                ],
+              },
+            ],
+            embeds: [
+              {
+                type: "rich",
+                title: "Minecraft Utilities",
+                description: "**There has been an error**",
+                color: 0xa92626,
+                fields: [
+                  {
+                    name: "Make sure to check the following things",
+                    value:
+                      "``` 1. The IP of the Server. \neg: play.yourserver.com ```\n``` 2. Make sure to use the right command.\n/Java for Java Edition, /Bedrok for Bedrock.```\n\n**Here are the reasons why your server might be having trouble**\n``` 1. Your Requested Server might be offline```\n``` 2. Your Provided IP might be Incorrect```\n``` 3. There are some issues with the bot, \nin that case, please use /help```", // eslint-disable-line
+                  },
+                ],
+              },
+            ],
+          });
+        }
+      });
+  },
 };
--- commands/mc-server/java.js
+++ commands/mc-server/java.js
@@ -1,143 +1,158 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const mcs = require('node-mcstatus');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 process.noDeprecation = true;
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('java')
-    .setDescription('Get Real-Time info about a Minecraft: Java Edition Server')
+    .setName("java")
+    .setDescription("Get Real-Time info about a Minecraft: Java Edition Server")
     .addStringOption((option) =>
       option
-        .setName('server-ip')
+        .setName("server-ip")
         .setRequired(true)
-        .setDescription('IP addess of the server'),
-    )
-    .addStringOption((option) =>
-      option
-      .setName('server-port')
-      .setDescription('Port of the server [Type Default for Default Port]')
-      .setRequired(true),
+        .setDescription("IP addess of the server")
     ),
-    async execute(interaction) {
 
-        await interaction.deferReply({ ephemeral: true });
+  async execute(interaction) {
+    await interaction.deferReply({ ephemeral: true });
 
-        let port = interaction.options.getString('server-port').toLowerCase();
-        const ip = interaction.options.getString('server-ip');
+    const ip = interaction.options.getString("server-ip");
+    const apiUrl = `https://api.mcsrvstat.us/3/${ip}`;
 
-        if (port == 'default')
-        {
-            port = 25565;
-        }
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.online) {
+          console.log("[JAVA] " +ip);
 
-        mcs.statusJava(ip, port)
-
-        .then((result) => {
-            console.log(ip + ', ' + port);
-
-            const javaEmbed = new EmbedBuilder()
-            .setColor(0x31D533)
-            .setThumbnail('attachment://icon.png')
-            .setTitle('Minecraft Utilities')
-            .setURL('https://spectex.xyz/projects/minecraft-utilities')
-            .setDescription('**Here is the requested server status**')
+          const javaEmbed = new EmbedBuilder()
+            .setColor(0x31d533)
+            .setThumbnail("attachment://icon.png")
+            .setTitle("Minecraft Utilities")
+            .setURL("https://spectex.xyz/projects/minecraft-utilities")
+            .setDescription("**Here is the requested server status**")
             .addFields(
-                { name: '**Players Online**', value: `\`\`\`${result.players.online}\`\`\`` },
-                { name: '**Max Player Cap**', value: `\`\`\`${result.players.max}\`\`\`` },
-                { name: '**Server Version**', value: `\`\`\`${result.version.name_clean}\`\`\`` },
-                { name: '**Online Mode**', value: `\`\`\`${result.online}\`\`\`` },
-                { name: '**Is The Eula Blocked ?**', value: `\`\`\`${result.eula_blocked}\`\`\`` },
-                { name: '**Mods, [ ] = No Mods**', value: `\`\`\`[${result.mods}]\`\`\`` },
-                { name: '**Motd**', value: `\`\`\`${result.motd.clean.trim()}\`\`\`` },
+              { name: "**Numeric IP**", value: `\`\`\`${data.ip}\`\`\`` },
+              { name: "**Server IP**", value: `\`\`\`${data.hostname}\`\`\`` },
+              {
+                name: "**Players Online - [Online / Max]**",
+                value: `\`\`\`${data.players.online} / ${data.players.max}\`\`\``,
+              },
+              { name: "**Version**", value: `\`\`\`${data.version}\`\`\`` },
+              {
+                name: "**Gamemode**",
+                value: `\`\`\`${data.gamemode || "Multiple"}\`\`\``,
+              },
+              {
+                name: "**EULA Blocked?**",
+                value: `\`\`\`${data.eula_blocked}\`\`\``,
+              },
+              {
+                name: "**Server Mode Online?**",
+                value: `\`\`\`${data.online}\`\`\``,
+              },
+              {
+                name: "**MOTD**",
+                value: `\`\`\`${data.motd.clean}\`\`\``,
+              }
             );
-            interaction.editReply({
-                files: [
-                    { attachment: Buffer.from(result.icon.split(',')[1], 'base64'),
-                    name: 'icon.png',
-                },
-                ],
-                components : [
+          interaction.editReply({
+            files: [
+              {
+                attachment: Buffer.from(data.icon.split(",")[1], "base64"),
+                name: "icon.png",
+              },
+            ],
+            components: [
+              {
+                type: 1,
+                components: [
                   {
-                    'type': 1,
-                    'components': [
-                      {
-                        'style': 5,
-                        'label': 'Vote',
-                        'url': 'https://top.gg/bot/810192936472936480/vote',
-                        'disabled': false,
-                        'type': 2,
-                      },
-                      {
-                        'style': 5,
-                        'label': 'Website',
-                        'url': 'https://spectex.xyz/projects/minecraft-utilities',
-                        'disabled': false,
-                        'type': 2,
-                      },
-                      {
-                        'style': 5,
-                        'label': 'Support Server',
-                        'url': 'https://discord.gg/jf28jcFJk9',
-                        'disabled': false,
-                        'type': 2,
-                      },
-                    ],
+                    style: 5,
+                    label: "Vote",
+                    url: "https://top.gg/bot/810192936472936480/vote",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Website",
+                    url: "https://spectex.xyz/projects/minecraft-utilities",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Support Server",
+                    url: "https://discord.gg/jf28jcFJk9",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Documentation",
+                    url: "https://mcutils.spectex.xyz",
+                    disabled: false,
+                    type: 2,
                   },
                 ],
-                embeds: [javaEmbed],
-            });
-            return;
-        })
-        .catch((error) => {
-            console.log('There has been an error');
-            interaction.editReply({
-              components: [
-                {
-                  'type': 1,
-                  'components': [
-                    {
-                      'style': 5,
-                      'label': 'Vote',
-                      'url': 'https://top.gg/bot/810192936472936480/vote',
-                      'disabled': false,
-                      'type': 2,
-                    },
-                    {
-                      'style': 5,
-                      'label': 'Website',
-                      'url': 'https://spectex.xyz/projects/minecraft-utilities',
-                      'disabled': false,
-                      'type': 2,
-                    },
-                    {
-                      'style': 5,
-                      'label': 'Support Server',
-                      'url': 'https://discord.gg/jf28jcFJk9',
-                      'disabled': false,
-                      'type': 2,
-                    },
-                  ],
-                },
-              ],
-              embeds: [
-                {
-                  'type': 'rich',
-                  'title': 'Minecraft Utilities',
-                  'description': '**There has been an error**',
-                  'color': 0xa92626,
-                  'fields': [
-                    {
-                      'name': 'Make sure to check the following things',
-                      'value': '\`\`\` 1. The IP of the Server. \neg: play.yourserver.com, 103.144.xxx.xxx \`\`\`\n\`\`\` 2. The PORT of the server. \neg: 25565 [Default].\`\`\`\n\`\`\` 3. Make sure to use the right command.\n/Java for Java Edition, /Bedrok for Bedrock.\`\`\`\n\n**Here are the reasons why your server might be having trouble**\n\`\`\` 1. Your Requested Server might be offline\`\`\`\n\`\`\` 2. Your Provided IP or PORT might be Incorrect\`\`\`\n\`\`\` 3. There are some issues with the bot, \nin that case, please use /help\`\`\`', // eslint-disable-line
-                    },
-                    {
-                      'name': 'Here is the error code',
-                      'value': `\`\`\`${error}\`\`\``,
-                    },
-                  ],
-                },
-              ],
-            });
-        });
-    },
+              },
+            ],
+            embeds: [javaEmbed],
+          });
+        } else {
+          interaction.editReply({
+            components: [
+              {
+                type: 1,
+                components: [
+                  {
+                    style: 5,
+                    label: "Vote",
+                    url: "https://top.gg/bot/810192936472936480/vote",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Website",
+                    url: "https://spectex.xyz/projects/minecraft-utilities",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Support Server",
+                    url: "https://discord.gg/jf28jcFJk9",
+                    disabled: false,
+                    type: 2,
+                  },
+                  {
+                    style: 5,
+                    label: "Documentation",
+                    url: "https://mcutils.spectex.xyz",
+                    disabled: false,
+                    type: 2,
+                  },
+                ],
+              },
+            ],
+            embeds: [
+              {
+                type: "rich",
+                title: "Minecraft Utilities",
+                description: "**There has been an error**",
+                color: 0xa92626,
+                fields: [
+                  {
+                    name: "Make sure to check the following things",
+                    value:
+                      "``` 1. The IP of the Server. \neg: play.yourserver.com ```\n``` 2. Make sure to use the right command.\n/Java for Java Edition, /Bedrok for Bedrock.```\n\n**Here are the reasons why your server might be having trouble**\n``` 1. Your Requested Server might be offline```\n``` 2. Your Provided IP might be Incorrect```\n``` 3. There are some issues with the bot, \nin that case, please use /help```", // eslint-disable-line
+                  },
+                ],
+              },
+            ],
+          });
+        }
+      });
+  },
 };
